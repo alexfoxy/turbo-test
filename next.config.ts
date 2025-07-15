8d5779e7997280e58fc7634fe1ff0e923d078be7
Alex Fox <alex@alexfox.dev>
2025-07-15 11:52:54 +0100

    upgrade next

--- next.config.ts
+++ next.config.ts
@@ -2,30 +2,7 @@ import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
   /* config options here */
-  transpilePackages: ['mobx'],
-  turbopack: {
-    rules: {
-      '*.ts': {
-        as: '*.js',
-        loaders: [
-          {
-            loader: 'babel-loader',
-            options: {
-              // presets: ['next/babel'],
-              plugins: [
-                ['@babel/plugin-proposal-decorators', {
-                  version: "2023-11",
-                  decoratorsBeforeExport: true
-                }],
-                '@babel/plugin-transform-runtime',
-                // '@babel/plugin-proposal-class-properties'
-              ]
-            }
-          }
-        ]
-      },
-    }
-  }
+  turbopack: {}
 }
 
 export default nextConfig
